3cd978796513bb82bec1c356c1e0591035f7d907
DanXside <danya.moiseev@inbox.ru>
2023-05-04 15:59:54 +1000

    main-feedback

--- test.php
+++ test.php
@@ -1,6 +1,20 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тест");
-?><p><br>
+?><p>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	 <br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 </p>
  <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
